08d95a5450ecb71f3da8ca9959e7fc65f54452cf
bambang16 <bambangnh16@gmail.com>
2017-04-10 23:25:08 +0700

    Add files via upload

---
+++ ab.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	cout<<"Mencari Nilai Max dan Min"<<endl;
+	//membandingkan nilai a dan b
+	cout<<"masukan nilai a = "; cin>>a;
+	cout<<"masukan nilai b = "; cin>>b;
+
+	if (a<b) {
+	cout<<"nilai max : "<<b;
+	}
+
+	else{ 
+	cout<<"nilai max : "<<a; 
+	}
+
+	return 0;
+} 
